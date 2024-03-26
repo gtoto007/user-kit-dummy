f74b575081e558a8189e86037fb8ddd31eccf7cd
Giuseppe Toto <billyball85@gmail.com>
2024-03-26 17:11:20 +0100

    Implement test case for serialization methods in UserDto

---
+++ tests/Unit/UserDtoTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Toto\UserKit\DTOs\UserDto;
+
+it('converts to array correctly', function () {
+    $user = new UserDto(1, 'test@example.com', 'First', 'Last', 'avatar.png');
+
+    $expected = [
+        'first_name' => 'First',
+        'last_name' => 'Last',
+        'avatar' => 'avatar.png',
+        'id' => 1
+    ];
+    expect($user->toArray())->toEqual($expected);
+});
+
+it('serializes to json correctly', function () {
+    $user = new UserDto(1, 'test@example.com', 'First', 'Last', 'avatar.png');
+
+    $expected = json_encode([
+        'first_name' => 'First',
+        'last_name' => 'Last',
+        'avatar' => 'avatar.png',
+        'id' => 1
+    ]);
+
+    expect(json_encode($user))->toEqual($expected);
+});
